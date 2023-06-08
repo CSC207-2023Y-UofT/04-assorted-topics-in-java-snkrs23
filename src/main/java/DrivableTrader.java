abf817b0422cbe82bf2a39f8f7660942931ed4bb
snkrs23 <80235486+snkrs23@users.noreply.github.com>
2023-06-08 01:49:33 -0400

    Update DrivableTrader.java

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -1,47 +1,20 @@
-/* TODO: Create a subclass of Trader named DrivableTrader
- * This class should be identical to Trader, except that it takes
- * only Drivable objects in its inventory, wishlist, etc.
- *
- * The sellingPrice returned should also be overridden. The selling price
- * should be equal to the:
- *     Object's price + Object's max speed
- * If the object is Tradable (and Tradable.MISSING_PRICE otherwise.)
- *
- * Look at DomesticatableTrader.java for an example.
- */
-
-import java.util.List;
-
-/* TODO: Create a subclass of Trader named DrivableTrader
- * This class should be identical to Trader, except that it takes
- * only Drivable objects in its inventory, wishlist, etc.
- *
- * The sellingPrice returned should also be overridden. The selling price
- * should be equal to the:
- *     Object's price + Object's max speed
- * If the object is Tradable (and Tradable.MISSING_PRICE otherwise.)
- *
- * Look at DomesticatableTrader.java for an example.
- */
-
-import java.util.List;
-
-/* A subclass of Trader named DomesticatableTrader.
- * This class is identical to Trader, except that it takes
- * only Domesticatable objects in its inventory, wishlist, etc.
- */
-
 import java.util.List;
 
 class DriveableTrader extends Trader<Driveable> {
     /**
-     * Construct a DomesticatableTrader, giving them the
+     * Construct a DriveableTrader, giving them the
      * given inventory, wishlist, and money.
      *
      * @param inventory Objects in this Trader's inventory
      * @param wishlist  Objects in this Trader's wishlist
      * @param money     The Trader's money
      */
+    public DriveableTrader(List<Driveable> inventory,
+                                List<Driveable>
+                                        wishlist,
+                                int money) {
+        super(inventory, wishlist, money);
+    }
 
     /**
      * Construct a DomesticatableTrader, giving them the
@@ -55,14 +28,14 @@ class DriveableTrader extends Trader<Driveable> {
 
     @Override
     public int getSellingPrice(Driveable item) {
-
-        if (item instanceof Tradable) {
-            Tradeable trade = (Tradeable) item;
-            return trade.getPrice() + trade.getMaxSpeed();
-        } else {
-            return Tradeable.MISSING_PRICE;
+        int super_price = super.getSellingPrice(item);
+        if (super_price == Tradable.MISSING_PRICE) {
+            return super_price;
         }
+
+        return super_price + item.getMaxSpeed();
 
     }
 
 }
+
